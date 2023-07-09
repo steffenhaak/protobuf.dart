4ff42f151a65deba1f2f5b7156c46d88054f8575
Kevin Moore <kevmoo@google.com>
2023-07-09 11:01:18 -0700

    fixes

--- protoc_plugin/lib/src/message_generator.dart
+++ protoc_plugin/lib/src/message_generator.dart
@@ -647,7 +647,7 @@ class MessageGenerator extends ProtobufContainer {
     IndentingWriter out,
     List<int> memberFieldPath,
   ) {
-    final fieldTypeString = field.getDartType();
+    var fieldTypeString = field.getDartType();
     final defaultExpr = field.getDefaultExpr();
     final names = field.memberNames;
 
@@ -697,7 +697,7 @@ class MessageGenerator extends ProtobufContainer {
             '${names.clearMethodName}() because it is repeated.';
       }
     } else {
-      final fastSetter = field.baseType.setter;
+      var fastSetter = field.baseType.setter;
       _emitDeprecatedIf(field.isDeprecated, out);
       _emitOverrideIf(field.overridesSetter, out);
       _emitIndexAnnotation(field.number, out);
